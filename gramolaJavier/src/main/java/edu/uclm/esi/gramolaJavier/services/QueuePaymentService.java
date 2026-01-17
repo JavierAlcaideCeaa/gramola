2b048367d6432f315ad45c83d517e9d2282ce12e
Javier Alcaide Cea <160161748+JavierAlcaideCeaa@users.noreply.github.com>
2026-01-17 17:50:34 +0100

    Change Stripe API key to placeholder
    Updated Stripe API key placeholder for security.

--- gramolaJavier/src/main/java/edu/uclm/esi/gramolaJavier/services/QueuePaymentService.java
+++ gramolaJavier/src/main/java/edu/uclm/esi/gramolaJavier/services/QueuePaymentService.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class QueuePaymentService {
     
     static {
-        Stripe.apiKey = "sk_test_51RJHbY00mVaZlVqd0NSRizvf088qWsP0Fh2yZyR3hnQ3kDTeSTX45vgaFC74bmENx21rPF7FzVpOgxDMZ1A89QcF00wcCu4W4Q";
+        Stripe.apiKey = "ponga su clave secreta";
     }
     
     @Autowired
